6a453e89e3794b4802ef42943f3b6ef12c024652
jean4876 <jaehoon940901@gmail.com>
2019-09-10 13:40:42 +0900

    spring프로젝트(스프링 컨테이너 생성)

---
+++ src/test/java/kr/or/ddit/test/board/dao/IBoardDao.java
@@ -0,0 +1,17 @@
+package kr.or.ddit.test.board.dao;
+
+public interface IBoardDao {
+	
+	/**
+	 * 
+	 * Method : getBoardList
+	 * 작성자 : PC-23
+	 * 변경이력 :
+	 * Method 설명 : 게시판 리스트를 출력한다.
+	 */
+	void getBoardList();
+	
+	
+	
+
+}
