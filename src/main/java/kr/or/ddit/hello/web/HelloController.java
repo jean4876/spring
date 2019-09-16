4a69357c214551c766c542eed8461712866c8ece
jean4876 <jaehoon940901@gmail.com>
2019-09-16 17:11:27 +0900

    spring controller 생성 	.@controller 	.@RequestMapping spring controller 테스트 	.WebApplicationContext (@Autowired) 	.MockMvc : dispatcherServlet
    * mockMvc 객체에 요청 url, 파라미터, 쿠키등 추가해서 요청을 보내면 spring framework를 통해 처리된
    ModelAndView 객체를 통해 정상처리 여부를 검증

--- src/main/java/kr/or/ddit/hello/web/HelloController.java
+++ src/main/java/kr/or/ddit/hello/web/HelloController.java
@@ -1,5 +1,11 @@
 package kr.or.ddit.hello.web;
 
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,16 +13,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 	
-	@RequestMapping("hello/hello.do")
-	public String hello(Model Model) {
-		
-		
-		//기존에 request에 넣었던 속성을  스프링에서는
-		//Model 객체에 넣는다
-		//request - > Model
-		//Session - > Session
-		//Application - > Application
-		return "hello/hello";
-	}
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+	@RequestMapping("hello/hello.do")
+	public String hello(Model Model, HttpServletRequest request) {
+		
+		logger.debug("hello()");
+
+		// 기존에 request에 넣었던 속성을 스프링에서는
+		// Model 객체에 넣는다
+		// request - > Model
+		// Session - > Session
+		// Application - > Application
+
+		Model.addAttribute("nowDt", new Date());
+		Model.addAttribute("msg", "hello, World");
+		Model.addAttribute("userId", request.getParameter("userId") + "_helloController");
+
+		return "hello/hello"; // /WEB-INF/views/hello/hello.jsp
+
+	}
 }
